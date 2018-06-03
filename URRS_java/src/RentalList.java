35f374cd5d13fc5256613e07ca5d67d6b4d2ac42
jjh <jjhn92@ajou.ac.kr>
2018-06-03 22:35:21 +0900

    push push baby

--- URRS_java/src/RentalList.java
+++ URRS_java/src/RentalList.java
@@ -1,14 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class RentalList {
 	private int quantity;
 	private Book book;
-	public RentalList(int bookId,int date) {
+	public RentalList(int bookId,int qty) {
 		BookList bookList = BookList.getInstance();
 		this.quantity = qty;
 		if(this.book.getAvailableQty() < qty) {
-			throw new RuntimeException ("")
+			throw new RuntimeException ("");
 		}
 	}
 	public int getQuantity() { return quantity;}

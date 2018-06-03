b54abbcb604a84cef74ba0112dfb6e31caff4c40
LeeSongA <thddk0317@nate.com>
2018-06-03 20:43:36 +0900

    edit code

--- URRS_java/src/BookState.java
+++ URRS_java/src/BookState.java
@@ -1,4 +1,4 @@
 
 public enum BookState {
-	Rentable,Rented,Booked // 대출가능, 대출중, 예약중
+	Rentable, Rented, Booked // 대출가능, 대출중, 예약중
 }
--- URRS_java/src/Rent.java
+++ URRS_java/src/Rent.java
@@ -1,5 +1,13 @@
 
 public class Rent {
+	Book book;
 	
+	public Rent(User user, Book book) {
+		this.book = book;
+	}
+	
+	public int getBookId() { 
+    	return book.bookId;
+    }
 
 }
